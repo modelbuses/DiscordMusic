963fa7985920ee29511bc2f0e79df025ae3df263
Darren <git@darrennathanael.com>
2022-03-09 04:26:09 +0100

    Logic was too good

--- kickstartReplit.sh
+++ kickstartReplit.sh
@@ -1,7 +1,9 @@
 # Because node:event error :p
 echo Kickstarting replit
+cat .replitBad >> .replit
 echo Started installing nodejs 16
 npm init -y && npm i --save-dev node@16 && npm config set prefix=$(pwd)/node_modules/node && export PATH=$(pwd)/node_modules/node/bin:$PATH
 npm rebuild
 npm i
 npm run deploy
+npm run start
