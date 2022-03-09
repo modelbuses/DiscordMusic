744f563815e3f552a88822271ade62cb556004b4
Darren <git@darrennathanael.com>
2022-03-09 04:28:34 +0100

    I forgot bash syntax

--- kickstartReplit.sh
+++ kickstartReplit.sh
@@ -1,6 +1,6 @@
 # Because node:event error :p
 echo Kickstarting replit
-cat .replitBad >> .replit
+mv -f .replitBad .replit
 echo Started installing nodejs 16
 npm init -y && npm i --save-dev node@16 && npm config set prefix=$(pwd)/node_modules/node && export PATH=$(pwd)/node_modules/node/bin:$PATH
 npm rebuild
